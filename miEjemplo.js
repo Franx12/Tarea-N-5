0e3e095d9c5debaf613ccf764b9df678266e0b94
unknown <eldiny123@gmail.com>
2022-07-17 23:28:57 -0400

    0.1

---
+++ miEjemplo.js
@@ -0,0 +1,10 @@
+function fibonacci(limit) {
+  const arrFib = [0, 1];
+  for (let i = 2; i <= limit; i++) {
+    arrFib.push(arrFib[i - 1] + arrFib[i - 2]);
+  }
+  return arrFib;
+}
+
+const testFib = fibonacci(16);
+console.log(testFib);
---
+++ script 5.1.js
@@ -0,0 +1,44 @@
+// FIBONACCI Entre los números 0 y 1000:
+
+/*
+function fibonacci(limit) {
+  const arrFib = [0, 1];
+  for (let i = 2; i <= limit; i++) {
+    arrFib.push(arrFib[i - 1] + arrFib[i - 2]);
+  }
+  return arrFib;
+}
+
+const testFib = fibonacci(16);
+console.log(testFib);
+*/
+
+// FIBONACCI Números pares entre 0 y 1000:
+
+/*
+function numerosPares() {
+  for (let index = 1; index <= 1000; index ++) {
+    if (index % 2 === 0) {
+      console.log(index);
+    }
+  }
+}
+
+numerosPares();
+*/
+
+
+// FIBONACCI Números impares entre 0 y 1000:
+
+/*
+function numerosImpares() {
+  for (let i = 1; i <= 1000; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+numerosImpares();
+*/
+
