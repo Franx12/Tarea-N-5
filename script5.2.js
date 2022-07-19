9d2136b8f4d9b816a5e505d2efbaf0c4806a17d7
unknown <eldiny123@gmail.com>
2022-07-18 21:29:27 -0400

    First Part

---
+++ script5.2.js
@@ -0,0 +1,19 @@
+//1.- Del siguiente arreglo de strings retornar otro arreglo con todo a mayúsculas:
+
+//let pokemones = ['Pikachu','Charmander','Bulbasaur','Squirtle']
+
+/*
+function misPokemones(n) {
+  let pokemones = ['Pikachu','Charmander','Bulbasaur','Squirtle'];
+  if (n < 1) {
+    return [pokemones[0].toUpperCase() + ", " + pokemones[1].toUpperCase() + ", " + pokemones[2].toUpperCase() + ", " + pokemones[3].toUpperCase()];
+  }
+}
+
+console.log(misPokemones(0));
+*/
+
+
+
+
+
