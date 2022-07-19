a7d2b1bd8d9fae0e731de1a64a2a7492188f9a6c
unknown <eldiny123@gmail.com>
2022-07-18 22:01:31 -0400

    Tarea N° 5

---
+++ script5.3.js
@@ -0,0 +1,73 @@
+//2.- Del siguiente arreglo de objetos, retornar otro arreglo con los pokemones tipo fuego. 
+
+/* let pokemones = [
+ {
+ nombre: 'Pikachu',
+ tipo: 'Electrico'
+ },
+ {
+ nombre: 'Charmander',
+ tipo: 'Fuego',
+ },
+ {
+ nombre: 'Bulbasaur',
+ tipo: 'Planta'
+ },
+ {
+ nombre: 'Squirtle',
+ tipo: 'Agua'
+ },
+ {
+ nombre: 'Charmeleon',
+ tipo: 'Fuego'
+ },
+ {
+ nombre: 'Weedle',
+ tipo: 'bicho'
+ },
+ {
+ nombre: 'Charizard',
+ tipo: 'Fuego'
+ }
+]
+*/
+
+/*
+function retornarPokemones(n) {
+	let pokemones = [
+    {
+    nombre: 'Pikachu',
+    tipo: 'Electrico'
+    },
+    {
+    nombre: 'Charmander',
+    tipo: 'Fuego',
+    },
+    {
+    nombre: 'Bulbasaur',
+    tipo: 'Planta'
+    },
+    {
+    nombre: 'Squirtle',
+    tipo: 'Agua'
+    },
+    {
+    nombre: 'Charmeleon',
+    tipo: 'Fuego'
+    },
+    {
+    nombre: 'Weedle',
+    tipo: 'bicho'
+    },
+    {
+    nombre: 'Charizard',
+    tipo: 'Fuego'
+    }
+   ];
+	if (n < 2) {
+		return [pokemones[1], pokemones[4], pokemones[6]];
+	}
+}
+
+console.log(retornarPokemones(0));
+*/
